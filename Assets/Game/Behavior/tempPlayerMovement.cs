f3ea75e1cf2b8ec4e53793a4a1a65d0e11a6131b
obriencole11 <obriencole11@gmail.com>
2016-01-29 22:20:09 -0500

    Intitial Commit

---
+++ Assets/Game/Behavior/tempPlayerMovement.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace game.behavior {
+public class tempPlayerMovement : MonoBehaviour {
+
+	public float speed = 10;
+	// Update is called once per frame
+	void Update () {
+			float h = Input.GetAxis ("Horizontal");
+			float v = Input.GetAxis ("Vertical");
+
+			gameObject.GetComponent<Rigidbody> ().AddForce (Vector3.right * h * Time.deltaTime * speed);
+			gameObject.GetComponent<Rigidbody> ().AddForce (Vector3.up * v * Time.deltaTime * speed);
+	}
+}
+}
