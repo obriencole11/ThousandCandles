02fc09c04d922256771b1cbd15d36eb8ebdbec00
obriencole11 <obriencole11@gmail.com>
2016-01-29 22:52:11 -0500

    Added a Basic Movement Controller

--- Assets/Game/Behavior/tempPlayerMovement.cs
+++ Assets/Game/Behavior/tempPlayerMovement.cs
@@ -4,14 +4,16 @@ using System.Collections;
 namespace game.behavior {
 public class tempPlayerMovement : MonoBehaviour {
 
-	public float speed = 10;
+	public float speed = 100;
 	// Update is called once per frame
 	void Update () {
 			float h = Input.GetAxis ("Horizontal");
 			float v = Input.GetAxis ("Vertical");
 
-			gameObject.GetComponent<Rigidbody> ().AddForce (Vector3.right * h * Time.deltaTime * speed);
-			gameObject.GetComponent<Rigidbody> ().AddForce (Vector3.up * v * Time.deltaTime * speed);
+			//gameObject.GetComponent<Rigidbody> ().AddForce (Vector3.right * h * Time.deltaTime * speed);
+			//gameObject.GetComponent<Rigidbody> ().AddForce (Vector3.forward * v * Time.deltaTime * speed);
+
+			transform.Translate (new Vector3( (h * Time.deltaTime * speed),(0) ,(v * Time.deltaTime * speed)) );
 	}
 }
 }
